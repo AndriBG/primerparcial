6be51835a182651b79b49e4652cbd28bdb8e471a
Andri Bonilla <guzmanbonilla7@gmail.com>
2022-02-21 08:03:45 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'presentacion',
+    loadChildren: () => import('./presentacion/presentacion.module').then( m => m.PresentacionPageModule)
   }
 ];
 
